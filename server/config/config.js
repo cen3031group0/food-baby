bf24ce5fb20f6c69fc224826533fa61510d18ab9
Victor Lin <vlin96@gmail.com>
2018-10-16 09:07:03 -0400

    Update URI for new DB
    #1

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 8080,
   db: {
-    uri: 'mongodb://CEN3031:CEN3031TA@ds161062.mlab.com:61062/listings',
+    uri: 'mongodb://CEN3031:CEN3031TA@ds161062.mlab.com:53459/foodaddy',
   }
 };
