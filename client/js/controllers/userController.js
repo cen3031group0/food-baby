b346b609e335d8d817f2eb7b9c587ceedd0eb6d4
Victor Lin <vlin96@gmail.com>
2018-11-08 19:39:54 -0500

    Remove unnecessary angular functions

--- client/js/controllers/userController.js
+++ client/js/controllers/userController.js
@@ -1,26 +1,15 @@
 angular.module('users').controller('UsersController', ['$scope', 'Users', 
   function($scope, Users) {
-    $scope.updateUsers = function() {
-      Users.getAll().then(function(response) {
-        $scope.users = response.data;
-      }, function(error) {
-        console.log('Unable to retrieve users:', error);
-      });
-    };
-    $scope.updateUsers();
-
     $scope.addUser = function() {
       $scope.user = {
         "name": $scope.newUser.name,
         "pass": $scope.newUser.pass
       }
       var res = Users.create($scope.user);
-      $scope.updateUsers();
     };
 
     $scope.deleteUser = function(id) {
       Users.delete(id);
-      $scope.updateUsers();
     };
   }
 ]);
--- client/js/factories/userFactory.js
+++ client/js/factories/userFactory.js
@@ -1,9 +1,5 @@
 angular.module('users', []).factory('Users', function($http) {
   var methods = {
-    getAll: function() {
-      return $http.get(window.location.origin + '/api/users');
-    },
-	
 	  create: function(user) {
 	    return $http.post(window.location.origin + '/api/users', user);
     }, 
